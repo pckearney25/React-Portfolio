21ec48199545cbf9ecc311c8ae28439d8a756687
Patrick Kearney <pckearney@yahoo.com>
2018-11-27 17:47:05 -0600

    Updates

--- precache-manifest.8b3f51dd6654baba8e160fdb3cedc85c.js
+++ precache-manifest.8b3f51dd6654baba8e160fdb3cedc85c.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/media/slider.468d4ab8.jpg"
   },
   {
-    "revision": "b8217bdb0950f5fc4ed6",
-    "url": "/React-Portfolio/static/css/main.7c371b52.chunk.css"
+    "revision": "e134677690c3df090a18",
+    "url": "/React-Portfolio/static/css/main.47871123.chunk.css"
   },
   {
     "revision": "03d909733d4672c916d0",
@@ -48,8 +48,8 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/media/portfolio.f48b4e2b.jpg"
   },
   {
-    "revision": "b8217bdb0950f5fc4ed6",
-    "url": "/React-Portfolio/static/js/main.b8217bdb.chunk.js"
+    "revision": "e134677690c3df090a18",
+    "url": "/React-Portfolio/static/js/main.e1346776.chunk.js"
   },
   {
     "revision": "3cf082b50afbfa49716ec9811b1861e6",
@@ -92,7 +92,7 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/media/rudbeckia.9df2ebf4.jpg"
   },
   {
-    "revision": "250fcc2d790e1ea780023e60be877130",
+    "revision": "cbf33c163ceac2a3dec6eee3dd349c31",
     "url": "/React-Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.4b4920d3dd1dcc70d1f9d6138d870742.js"
+  "/React-Portfolio/precache-manifest.8b3f51dd6654baba8e160fdb3cedc85c.js"
 );
 
 workbox.clientsClaim();
