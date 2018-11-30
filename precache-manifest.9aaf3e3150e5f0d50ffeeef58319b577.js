2dd2986eecf4507dfe5732aa36e5589b914192e7
Patrick Kearney <pckearney@yahoo.com>
2018-11-30 14:38:46 -0600

    Updates

--- precache-manifest.9aaf3e3150e5f0d50ffeeef58319b577.js
+++ precache-manifest.9aaf3e3150e5f0d50ffeeef58319b577.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/media/slider.468d4ab8.jpg"
   },
   {
-    "revision": "adc32a5020ceff50b3f1",
+    "revision": "0df1889798de2b47fbfa",
     "url": "/React-Portfolio/static/css/main.e25b059a.chunk.css"
   },
   {
@@ -48,8 +48,8 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/media/portfolio.f48b4e2b.jpg"
   },
   {
-    "revision": "adc32a5020ceff50b3f1",
-    "url": "/React-Portfolio/static/js/main.adc32a50.chunk.js"
+    "revision": "0df1889798de2b47fbfa",
+    "url": "/React-Portfolio/static/js/main.0df18897.chunk.js"
   },
   {
     "revision": "3cf082b50afbfa49716ec9811b1861e6",
@@ -92,7 +92,7 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/media/rudbeckia.9df2ebf4.jpg"
   },
   {
-    "revision": "f4cd0d354a52527488364710b40cf580",
+    "revision": "d5c2f1284a8135d6e47c7c038525d227",
     "url": "/React-Portfolio/index.html"
   }
 ];
